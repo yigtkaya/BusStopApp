cdeb503841af76ada874b5d0eb47e5b57e13c769
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-10-02 20:16:09 +0300

    implementation of dagger neccessary application class and get stations from api

--- app/src/main/java/com/example/voltlinescasestudy/data/repository/StationsRepositoryImpl.kt
+++ app/src/main/java/com/example/voltlinescasestudy/data/repository/StationsRepositoryImpl.kt
@@ -1,18 +1,50 @@
 package com.example.voltlinescasestudy.data.repository
 
+import com.example.voltlinescasestudy.data.remote.StationsApi
 import com.example.voltlinescasestudy.domain.models.Station
 import com.example.voltlinescasestudy.domain.repository.StationsRepository
 import com.example.voltlinescasestudy.util.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class StationsRepositoryImpl : StationsRepository {
+@Singleton
+class StationsRepositoryImpl @Inject constructor(
+    private val stationsApi: StationsApi
+): StationsRepository {
+
     override suspend fun getStations(): Flow<Resource<List<Station>>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(Resource.Loading(true))
+            val stationListing = try {
+                val response = stationsApi.getStations()
+
+                if(response.isSuccessful) {
+                    response.body()?.stations ?: emptyList()
+                } else {
+                    emptyList()
+                }
+            } catch (e: IOException) {
+                emit(Resource.Error(e.message.toString()))
+                return@flow
+            } catch (e: HttpException) {
+                emit(Resource.Error(e.message.toString()))
+                return@flow
+            }
+
+            stationListing.let {
+                emit(Resource.Success(
+                    data = stationListing
+                ))
+                emit(Resource.Loading(false))
+            }
+        }
     }
 
     override suspend fun bookTrip() {
         TODO("Not yet implemented")
     }
-
-
 }
